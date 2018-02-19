ab0d66b4877c38fe8a69b05d48c1dfb458afc87a
parth-shel <ps@parthshel.com>
2018-02-18 22:19:12 -0500

    refactored server logs

---
+++ xpac-sever/src/server-log.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+#include "server-log.hh"
+
+using namespace std;
+
+namespace log {
+	//TODO: initialize log file if it does not exist and record server start time
+	extern int initialize_log(const char * file_name) {
+		log_file_name = string(file_name);
+		if(file_exists(file_name)) { // log file exists
+			ofstream log_file(file_name, ios::out | ios::app);
+			if(log_file.is_open()) {
+				log_file << get_current_time() << "\t---.---.---.---\t\tserver started\n";
+			}
+			else {
+				return -1;
+			}
+
+		}
+		else { // need to create a new file
+			ofstream log_file(file_name, ios::out | ios::trunc);
+			if(log_file.is_open()) {
+				log_file << "TIME\t\t\t\tIP\t\t\tREQUEST\n";
+				log_file << get_current_time() << "\t---.---.---.---\t\tserver started\n";
+			}
+			else {
+				return -1;
+			}			
+		}
+		return 0;
+	}
+
+	//TODO: record time of request and IP of client
+	extern int log_event(char * IP, char * request) {
+		if(file_exists(log_file_name.c_str())) {
+			ofstream log_file(log_file_name.c_str(), ios::out | ios::app);
+			if(log_file.is_open()) {
+				log_file << get_current_time() << "\t" << IP << "\t" << request << "\n";
+			}
+			else {
+				return -1;
+			}
+		}
+		else {
+			return -1;
+		}
+	}
+
+	inline bool file_exists(const char * file_name) {
+		std::ifstream f(file_name);
+		return f.good();
+	}
+
+	inline string get_current_time() {
+		// current date/time based on current system
+   		time_t now = time(0);
+   
+		string time_string = ctime(&now);
+		time_string.erase(time_string.length()-1, 1);
+
+   		// convert now to string form
+   		return time_string;
+	}
+}
---
+++ xpac-sever/src/server-log.hh
@@ -0,0 +1,18 @@
+#ifndef log_hh
+#define log_hh
+
+#include <string>
+
+namespace log {
+	static std::string log_file_name;
+
+	extern int initialize_log(const char * file_name);
+
+	extern int log_event(char * IP, char * request);
+	
+	inline bool file_exists(const char * file_name);
+
+	inline std::string get_current_time();
+}
+
+#endif
