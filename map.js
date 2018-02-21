56a00aa2d2c8c35fa5f157e58bba34122e330562
Anunai Ishan <aishan@purdue.edu>
2018-02-21 18:49:19 -0500

    Fix visibility issues with embedded maps

--- map.js
+++
@@ -1,9 +0,0 @@
-function map() {
-	var mapCenter = new google.maps.LatLng(40.4259, -86.9081);
-	var properties = {
-		center : mapCenter,
-		zoom : 12, scrollwheel : false, draggable : false,
-		mapTypeId : google.maps.mapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("locationMap"), properties);
-}
