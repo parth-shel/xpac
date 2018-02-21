83214652bc1b8ad6317b3edb4e90241f3d42ba97
Amol Moses Jha <amolmoses@gmail.com>
2018-02-21 13:07:51 -0500

    Still trying to get file IO to work

--- metadata/metadata.cpp
+++ metadata/metadata.cpp
@@ -11,32 +11,27 @@ metadata::metadata(std::string pkg_name, int size){
 std::string metadata::get_info(){
 	std::string info;
 	info += pkg_name + "\n" + std::to_string(size) + "\n" + pkg_id + "\n";
-	for(auto && itr=dep_list->begin(); itr!=dep_list->end(); itr++)
-		info += *itr+";";
+	if(dep_list->size()>0 && dep_list!=NULL)
+		for(auto && itr=dep_list->begin(); itr!=dep_list->end(); itr++)
+			info += *itr+";";
 	return info;
 }
 
 std::string metadata::write_package(metadata * package){
 	std::ofstream file_object;
 	std::string filepath = package->pkg_id;
-	file_object.open(filepath);
-	file_object << package->get_info();
+	file_object.open(filepath.c_str(),std::ios::out | std::ios::binary);
+	file_object.write((char*)package, sizeof(package));
 	file_object.close();
 	return filepath;
 }
 
 metadata * metadata::get_package(std::string filepath){
 	std::ifstream file_object;
-	file_object.open(filepath.c_str());
-	metadata * package = new metadata();
-	std::string line_read_arr[4];
-	std::string next_line;
-	int i = 0;
-	while(getline(file_object,next_line)){
-		line_read_arr[i++] = next_line;
-	}
-	package->pkg_name = line_read_arr[0];
-	package->size = 	
+	file_object.open(filepath.c_str(),std::ios::in | std::ios::binary);
+	metadata * new_package = (metadata*)malloc(sizeof(metadata));
+	file_object.read((char*)new_package, sizeof(metadata));
+	new_package->get_info();
 	file_object.close();
-	return package;
+	return new_package;
 }
