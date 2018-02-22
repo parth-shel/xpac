fec8edaedd5496397112252ab55ba9870d22c665
Anunai Ishan <aishan@purdue.edu>
2018-02-21 21:57:16 -0500

    Implement man help

---
+++ client-src/man_help.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+extern void print_err() {
+	cout<<"These are common xpac commands used in various situations:"<<endl<<endl;
+	cout<<"xpac -install <package_name>\tInstall a package including its required dependencies"<<endl;
+	cout<<"install <package_name>\tInstall a package including its required dependencies"<<endl;
+	cout<<"install <package_name>\tInstall a package including its required dependencies"<<endl;
+	cout<<"install <package_name>\tInstall a package including its required dependencies"<<endl;
+	cout<<"install <package_name>\tInstall a package including its required dependencies"<<endl;
+	cout<<"install <package_name>\tInstall a package including its required dependencies"<<endl;
+
+}
