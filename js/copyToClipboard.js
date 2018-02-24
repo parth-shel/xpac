bd5d0d6b19cfcc0b31c0098d233ea66523d3f1cc
Amol Moses Jha <amolmoses@gmail.com>
2018-02-23 19:55:26 -0500

    Remove obsolete dummy method

--- js/copyToClipboard.js
+++ js/copyToClipboard.js
@@ -3,38 +3,25 @@ $(document).ready(function() {
 	// copy command to clipboard button is clicked
 	$("#button1").click(function() {
 		var command = $("#download-cmd").text();
+		$("#command1").val(command);
 		console.log(command);
-		dummy(command);
 	});
 
 	$("#button2").click(function() {
 		var command = $("#open-dir-cmd").text();
+		$("#command2").val(command);
 		console.log(command);
-		dummy(command);
 	});
 
 	$("#button3").click(function() {
 		var command = $("#make-cmd").text();
+		$("#command3").val(command);
 		console.log(command);
-		dummy(command);
 	});
 
 	$("#button4").click(function() {
 		var command = $("#install-cmd").text();
+		$("#command4").val(command);
 		console.log(command);
-		dummy(command);
 	});
-
-	// dummy method which creates an input field to copy text to clipboard from
-	function dummy(command) {
-		var dummyInput = document.createElement("input");
-		document.body.appendChild(dummyInput);
-		$(dummyInput).css('display', 'none');
-		dummyInput.setAttribute("id", "dummy_id");
-		document.getElementById("dummy_id").value = command;
-		dummyInput.select();
-		document.execCommand("copy");
-		document.body.removeChild(dummyInput);
-		console.log("Successfully copied!");
-	}
 });
