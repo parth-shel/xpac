43533c8b280f18507ed5c213e9ec1e0006cdd0ba
Anunai Ishan <ishananunai@gmail.com>
2018-02-24 13:16:02 -0500

    Fail fast, fail often

--- js/copyToClipboard.js
+++ js/copyToClipboard.js
@@ -4,24 +4,29 @@ $(document).ready(function() {
 	$("#button1").click(function() {
 		var command = $("#download-cmd").text();
 		$("#command1").val(command);
-		console.log(command);
+		$("#command1").select();
+		document.execCommand("Copy");
+		// $("#command1").css("display", "none");
 	});
 
 	$("#button2").click(function() {
 		var command = $("#open-dir-cmd").text();
 		$("#command2").val(command);
-		console.log(command);
+		$("#command2").select();
+		document.execCommand("Copy");
 	});
 
 	$("#button3").click(function() {
 		var command = $("#make-cmd").text();
 		$("#command3").val(command);
-		console.log(command);
+		$("#command3").select();
+		document.execCommand("Copy");
 	});
 
 	$("#button4").click(function() {
 		var command = $("#install-cmd").text();
 		$("#command4").val(command);
-		console.log(command);
+		$("#command4").select();
+		document.execCommand("Copy");
 	});
 });
