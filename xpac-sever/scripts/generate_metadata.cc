5207837435d814ab5fd0d2a1051f11a70cbbd443
parth-shel <ps@parthshel.com>
2018-03-18 20:29:31 -0400

    working on script to generate metadata

--- xpac-sever/scripts/generate_metadata.cc
+++ xpac-sever/scripts/generate_metadata.cc
@@ -1,1 +1,31 @@
-vsddghcvscvvbddbvsc sdc
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string pkg_name;
+	unsigned long long pkg_ID;
+	string buf;
+
+	vector<string> dependency_list;
+
+	cout << "Enter package name: ";
+	cin >> pkg_name;
+
+	cout << "Enter package ID: ";
+	cin >> pkg_ID;
+
+	cout << "Enter package ID's of dependencies (enter '-1' when you're done): ";
+	while(buf.compare("-1") == 0) {
+		cin >> buf;
+		if(buf.compare("-1") != 0)
+			dependency_list.push_back(buf);
+	}
+
+	//write to file
+
+
+	return EXIT_SUCCESS;
+}
