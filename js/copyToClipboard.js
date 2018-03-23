35c828533625b8aaf28fdcfa48473ee4366861dd
Anunai Ishan <ishananunai@gmail.com>
2018-03-22 20:55:28 -0400

    Implement functionality for copying commands to clipboard

--- js/copyToClipboard.js
+++ js/copyToClipboard.js
@@ -1,32 +1,11 @@
 $(document).ready(function() {
-
-	// copy command to clipboard button is clicked
-	$("#button1").click(function() {
-		var command = $("#download-cmd").text();
-		$("#command1").val(command);
-		$("#command1").select();
-		document.execCommand("Copy");
-		// $("#command1").css("display", "none");
-	});
-
-	$("#button2").click(function() {
-		var command = $("#open-dir-cmd").text();
-		$("#command2").val(command);
-		$("#command2").select();
-		document.execCommand("Copy");
-	});
-
-	$("#button3").click(function() {
-		var command = $("#make-cmd").text();
-		$("#command3").val(command);
-		$("#command3").select();
-		document.execCommand("Copy");
-	});
-
-	$("#button4").click(function() {
-		var command = $("#install-cmd").text();
-		$("#command4").val(command);
-		$("#command4").select();
-		document.execCommand("Copy");
-	});
+	if(!ClipboardJS.isSupported()) {// hide copy-to-clipboard buttons
+		$("#button1").hide();
+		$("#button2").hide();
+		$("#button3").hide();
+		$("#button4").hide();
+	}
+	
+	// create an instance of ClipboardJS
+	var clipboard = new ClipboardJS('.btn');
 });
