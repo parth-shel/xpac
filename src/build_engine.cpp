c7d7a28e0907b28bc4c8541b86a62ceb6e51199e
parth-shel <ps@parthshel.com>
2018-03-24 00:36:58 -0400

    fixed build engine

--- src/build_engine.cpp
+++ src/build_engine.cpp
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <unistd.h>
 #include <sys/types.h>
 
 int build(std::string command) {
 	int current_uid = getuid();
-	printf("My UID is: %d. My GID is: %d\n", current_uid, getgid());
-	system("%s", command.c_str());
 	if (seteuid(0)) {
 		perror("setuid");
 		return -1;
 	}
-	//I am now root!
-	printf("My UID is: %d. My GID is: %d\n", getuid(), getgid());
-	system("%s", command.c_str());
-	//Time to drop back to regular user privileges
+	system(command.c_str());
 	seteuid(current_uid);
-	printf("My UID is: %d. My GID is: %d\n", getuid(), getgid());
-	system("%s", command.c_str());
 	return 0;
 }
 
-int main(int argc, char * argv[]) {
+/*int main(int argc, char * argv[]) {
 	std::string command("/usr/bin/id");
 
 	if( build(command) < 0 ) {
@@ -29,4 +23,4 @@ int main(int argc, char * argv[]) {
 	}
 
 	return EXIT_SUCCESS;
-}
+}*/
