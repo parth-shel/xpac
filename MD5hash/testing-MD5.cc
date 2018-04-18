339e2770ab14f745cf2dd68b32f162825b8f4495
parth-shel <ps@parthshel.com>
2018-04-18 01:19:42 -0400

    working on testing the MD5 checksum module

--- MD5hash/testing-MD5.cc
+++ MD5hash/testing-MD5.cc
@@ -8,10 +8,10 @@
 #include "MD5checksum.hh"
 
 int main(int argc, char* argv[]) {
-	if(argc != 3) {
+	/*if(argc != 3) {
 		std::cout << "please specify file path" << std::endl;
 		exit(EXIT_FAILURE);
-	}
+	}*/
 
 	MD5checksum* md51 = new MD5checksum(std::string("test1.txt"));
 	MD5checksum* md52 = new MD5checksum(std::string("test2.txt"));
@@ -19,14 +19,17 @@ int main(int argc, char* argv[]) {
 
 	assert(md51->compare_hashes(md52));
 	std::cout << "files 1 and 2 are the same!" << std::endl;
+	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
 	assert(!(md51->compare_hashes(md53)));
 	std::cout << "files 1 and 3 differ.." << std::endl;
-
+	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
+	
 	std::string hash_path(std::string("test2.txt"));
 	hash_path.append(".md5hash");
 	md52->save_hash();
 	assert(md51->compare_saved_hash(hash_path));
 	std::cout << "saved hash matches for files 1 and 2!" << std::endl;
+	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
 
 	delete md51;
 	delete md52;
