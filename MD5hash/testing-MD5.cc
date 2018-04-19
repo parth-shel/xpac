6074fc05d0d29497151520bc78af2d78636d9e7d
parth-shel <ps@parthshel.com>
2018-04-18 23:44:59 -0400

    finishing up the test cases

--- MD5hash/testing-MD5.cc
+++ MD5hash/testing-MD5.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <cassert>
+#include <cstdlib>
 
 // uncomment to disable assert()
 // #define NDEBUG
@@ -19,6 +20,7 @@ int main(int argc, char* argv[]) {
 	MD5checksum* md54 = new MD5checksum(std::string("badtest.txt"));
 	MD5checksum* md55 = new MD5checksum(std::string("complexTest"));
 	
+	std::cout << "\033[1;33m Test Case to check whether the same hash is calculated for similar files \033[0m" << std::endl;
 	assert(md51->compare_hashes(md52));
 	std::cout << "files 1 and 2 are the same!" << std::endl;
 	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
@@ -26,6 +28,7 @@ int main(int argc, char* argv[]) {
 	std::cout << "files 1 and 3 differ.." << std::endl;
 	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
 	
+	std::cout << "\033[1;33m Test Case to check whether diferrent hash is calculated for different files \033[0m" << std::endl;
 	std::string hash_path(std::string("test2.txt"));
 	hash_path.append(".md5hash");
 	md52->save_hash();
@@ -33,17 +36,25 @@ int main(int argc, char* argv[]) {
 	std::cout << "saved hash matches for files 1 and 2!" << std::endl;
 	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
 	
+	std::cout << "\033[1;33m Test Case to check whether the MD5 hashing module is robust to handle file I/O errors \033[0m" << std::endl;
 	assert(!(md54->compare_hashes(md51)));
 	std::cout << "file 4 I/O error.." << std::endl;
 	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
 	
+	std::cout << "\033[1;33m Test Case to check whether the module can handle complex and large files \033[0m" << std::endl;
 	md55->save_hash();
-
+    delete md55;
+    md55 = new MD5checksum(std::string("complexTest"));
+    std::string complex_hash("complexTest.md5hash");
+    assert(md55->compare_saved_hash(complex_hash));
+    std::cout << "complex hashes match" << std::endl;
+	std::cout << "\033[1;32m Test Case Passed \033[0m" << std::endl;
 
 	delete md51;
 	delete md52;
 	delete md53;
 	delete md54;
+	delete md55;
 
 	return EXIT_SUCCESS;
 }
